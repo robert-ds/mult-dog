cbf0d270a740ec84b7c3924b95bf884d44ec0f0b
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-04 13:08:39 -0400

    button for new dogs ok

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const URL = 'https://api.thedogapi.com/v1/images/search';
+const newImg = document.querySelector("#btn");
 
-fetch(URL)
+newImg.addEventListener("click", () => {
+  dogs();
+});
+
+
+const dogs = () => {
+  fetch(URL)
   .then(res => res.json())
   .then(data => {
     const img = document.querySelector('img');
     img.src = data[0].url;
   });
+} 
